c0d6c1da49de5f10845e67d0fb8900dc7dbcdad3
Vincent Engel <vincentengelsoftware@gmail.com>
2024-05-12 09:15:54 +0200

    Fix SymfonyQueryBus

--- src/Shared/Port/Bus/Query/SymfonyQueryBus.php
+++ src/Shared/Port/Bus/Query/SymfonyQueryBus.php
@@ -8,10 +8,16 @@ use App\Shared\Application\Bus\Query\Query;
 use App\Shared\Application\Bus\Query\QueryBus;
 use App\Shared\Application\Bus\Query\QueryResponse;
 use Symfony\Component\Messenger\HandleTrait;
+use Symfony\Component\Messenger\MessageBusInterface;
 
 class SymfonyQueryBus implements QueryBus
 {
     use HandleTrait; // Using trait because ->handle() returns mixed
+
+    public function __construct(MessageBusInterface $messageBus)
+    {
+        $this->messageBus = $messageBus;
+    }
 
     public function ask(Query $query): QueryResponse
     {
